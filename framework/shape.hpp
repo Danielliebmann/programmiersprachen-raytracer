f6bffbffa6e7cc6c80c50d274fb9961654b3ae5a
Daniel Liebmann <nelu8763@billl1.in.uni-weimar.de>
2016-06-08 15:23:31 +0200

    5.1 GG EZ

---
+++ framework/shape.hpp
@@ -0,0 +1,37 @@
+#ifndef SHAPE_HPP
+#define SHAPE_HPP
+#include <math.h>
+#include <cmath>
+
+//Eine virtuelle Methode ist in C++ eine Methode einer Klasse, deren Einsprungadresse erst zur Laufzeit ermittelt wird.
+
+//5.1 Shape Klasse
+//Pure Virtual Methoden
+class Shape{ 
+public:
+	virtual void area() const = 0; //= 0 auch bekannt als 'pure specifier'
+	virtual void volume() const = 0;
+	};
+
+
+
+
+
+
+
+//5.4
+class Shape
+{
+public:
+	//...
+	virtual std::ostream& print(std::ostream& os) const;
+
+	//...
+};
+
+std::ostream& operator<<(std::ostream& os, Shape const& s)
+{
+	//not implemented yet
+}
+
+vr vr2016_buw_ss
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,13 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 
+TEST_CASE("TEST1", "x")
+{
+	SECTION("Hagrids Hundehuette")
+
+
+
+
 int main(int argc, char *argv[])
 {
   return Catch::Session().run(argc, argv);
